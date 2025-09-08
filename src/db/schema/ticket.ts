cb938039509e94e7195faa5b3b15aac2488b1a0a
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-09-08 22:15:18 +0100

    feat: create ticket schema with necessary fields and relationships

---
+++ src/db/schema/ticket.ts
@@ -0,0 +1,17 @@
+import { pgTable, uuid, varchar, text, timestamp } from 'drizzle-orm/pg-core';
+import { event } from './event';
+import { user } from './auth-schema';
+import { booking } from './booking';
+
+export const ticket = pgTable('tickets', {
+  id: uuid('id').primaryKey(),
+  eventId: uuid('event_id')
+    .notNull()
+    .references(() => event.id, { onDelete: 'no action' }),
+  userId: text('user_id')
+    .notNull()
+    .references(() => user.id, { onDelete: 'no action' }),
+  bookingId: uuid('booking_id')
+    .notNull()
+    .references(() => booking.id, { onDelete: 'cascade' }),
+});
