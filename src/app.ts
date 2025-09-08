074dfbc420069077f1064804a5103b812eb7c0b1
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-09-08 10:49:24 +0100

    feat: integrate Swagger for API documentation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,28 @@ import express, { Application } from 'express';
 import routes from './routes';
 import pinoHttp from 'pino-http';
 import logger from './utils/logger';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const swaggerOptions: swaggerJSDoc.Options = {
+  definition: {
+    info: {
+      title: 'Event ticketing Express API with Swagger',
+      version: '1.0.0',
+      description:
+        'An CRUD app to book event tickets made in express and documented \
+        with Swagger',
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000',
+      },
+    ],
+  },
+  apis: ['./src/features/**/*.ts'],
+};
+
+const specs = swaggerJSDoc(swaggerOptions);
 
 const app: Application = express();
 app.use(
@@ -9,6 +31,7 @@ app.use(
     logger,
   })
 );
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use(express.json());
 
