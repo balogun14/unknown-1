6f0df709f58f99a0f9dd69f84755b1629cdf388d
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-09-08 09:29:43 +0100

    fix: correct import name for event management routes in index file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import authRoutes from '../features/auth/components/auth-routes';
-import eventRoutes from '../features/event-management/components/event-routes';
+import eventManagementRoutes from '../features/event-management/components/event-routes';
 
 const router: Router = Router();
+
 router.use('/auth', authRoutes);
-router.use('/', eventRoutes);
+router.use('/', eventManagementRoutes);
 
 export default router;
