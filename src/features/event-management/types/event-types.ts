ecae1ec64a79772b92e6d2a165d212816c1682e0
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-09-08 09:30:05 +0100

    feat: define Zod schemas for event creation and update validation

--- src/features/event-management/types/event-types.ts
+++ src/features/event-management/types/event-types.ts
@@ -1,14 +1,18 @@
+import z from 'zod';
 import { event } from '../../../db/schema/event';
 
-// export interface CreateEventDto {
-//   name: string;
-//   venue: string;
-//   description: string;
-//   startDate: Date;
-//   price: number;
-// }
+export const createEventSchema = z.object({
+  name: z.string().min(6).trim(),
+  venue: z.string().min(6),
+  description: z.string().min(1).trim(),
+  startDate: z.iso.datetime(),
+  price: z.number().nonnegative(),
+});
+export const updateEventSchema = createEventSchema;
 
 export type CreateEventDto = Omit<
   typeof event.$inferInsert,
   'id' | 'createdAt' | 'updatedAt' | 'createdBy'
 >;
+
+export type UpdateEventDto = CreateEventDto;
