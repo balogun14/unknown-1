37bdf1ff0387a6f045e6702920c8613d109b82db
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-09-09 16:21:17 +0100

    refactor: reorganize booking routes import for improved clarity

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import authRoutes from '../features/auth/components/auth-routes';
 import eventManagementRoutes from '../features/event-management/components/event-routes';
-
+import bookingRoutes from '../features/booking/components/booking-routes';
 const router: Router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/', eventManagementRoutes);
-
+router.use('/', bookingRoutes);
 export default router;
